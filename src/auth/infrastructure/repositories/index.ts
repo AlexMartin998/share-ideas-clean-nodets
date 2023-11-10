edda97c9db47c9d0af7b83b35ea1d7675fa78934
Adrian <alextravc.342@gmail.com>
2023-11-10 15:50:48 -0500

    [ADD]: UserRepositoryImpl (infra)

---
+++ src/auth/infrastructure/repositories/index.ts
@@ -0,0 +1,2 @@
+export * from './role.repository.impl';
+export * from './user.repository.impl';
---
+++ src/auth/infrastructure/repositories/user.repository.impl.ts
@@ -0,0 +1,36 @@
+import { UserDatasource } from "@/auth/domain/datasources";
+import { User } from "@/auth/domain/models";
+import { UserRepository } from "@/auth/domain/repositories";
+
+
+export class UserRepositoryImpl implements UserRepository {
+
+  ///* DI
+  constructor(private readonly userDatasource: UserDatasource) {}
+
+
+  findAll(): Promise<User[]> {
+    return this.userDatasource.findAll();
+  }
+
+  findOne(id: number): Promise<User> {
+    return this.userDatasource.findOne(id);
+  }
+
+  findOneByEmail(email: String): Promise<User> {
+    return this.userDatasource.findOneByEmail(email);
+  }
+
+  create(user: User): Promise<User> {
+    return this.userDatasource.create(user);
+  }
+
+  update(id: number, user: User): Promise<User> {
+    return this.userDatasource.update(id, user);
+  }
+
+  delete(id: number): Promise<boolean> {
+    return this.userDatasource.delete(id);
+  }
+
+}
