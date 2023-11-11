f143b56a7969e61739f41162da1d56074003e50b
Adrian <alextravc.342@gmail.com>
2023-11-11 11:11:44 -0500

    [ADD]: UserRegistrator validate that email is not registerd

--- src/auth/application/use-cases/user-registrator.usecase.ts
+++ src/auth/application/use-cases/user-registrator.usecase.ts
@@ -1,4 +1,5 @@
 import { JwtConstants } from '@/auth/application/constants';
+import { UserAlreadyExistError } from '@/auth/domain/errors';
 import { User } from '@/auth/domain/models';
 import { UserRepository } from '@/auth/domain/repositories';
 import {
@@ -6,6 +7,7 @@ import {
   RegisterUser,
   UserToken,
 } from '@/auth/domain/use-cases';
+import { InvalidArgumentError } from '@/shared/domain';
 
 
 export class UserRegistrator implements RegisterUser {
@@ -17,14 +19,18 @@ export class UserRegistrator implements RegisterUser {
   ) {}
 
 
-  async register(user: User): Promise<UserToken> {
+  async run(user: User): Promise<UserToken> {
+    const userSaved = await this.userRepository.findOneByEmail(user.email);
+    if (userSaved) throw new UserAlreadyExistError('Email already registered');
+
     const newUser: User = await this.userRepository.create(user);
 
     const token = await this.authTokenHandler.generateToken(
       { id: newUser.id },
       JwtConstants.duration
     );
-    if (!token) throw new Error('Something went wrong while creating JWT');
+    if (!token)
+      throw new InvalidArgumentError('Something went wrong while creating JWT');
 
     return {
       token,
