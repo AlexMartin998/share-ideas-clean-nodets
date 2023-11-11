2f63151090529a779d454855372f00563332bd5f
Adrian <alextravc.342@gmail.com>
2023-11-10 23:01:53 -0500

    [ADD]: JwtConstants (infra)

--- src/auth/infrastructure/mappers/user.mapper.ts
+++ src/auth/infrastructure/mappers/user.mapper.ts
@@ -4,22 +4,34 @@ import { UserResponseDto } from '../dtos';
 
 export class UserMapper {
 
+  // would manages the domain event registration
+  public static entityToDomain(object: { [key: string]: any }): User {
+    const { id, username, email, password, role } = object;
+
+    return User.create({ id, username, email, password, role });
+  }
+  
+
   public static entityToDomainModel(object: { [key: string]: any }): User {
     const { id, username, email, password, role } = object;
 
-    return new User(id, username, email, password, role);
+    return User.create({ id, username, email, password, role });
   }
-
-  public static domainModelToResponseDto(user: User) {
-    return UserResponseDto.create(user);
-  }
-
+  
   public static createUserDtoToDomainModel(object: {
     [key: string]: any;
   }): User {
     const { id, username, email, password, role } = object;
 
-    return User.create({ id, username, email, password, role });
+    return new User(id, username, email, password, role);
+  }
+
+  public static domainModelToResponseDto(object: {
+    [key: string]: any;
+  }): UserResponseDto {
+    const { user, token } = object;
+
+    return UserResponseDto.create({ user, token });
   }
 
 }
--- src/shared/insfrastructure/config/envs.ts
+++ src/shared/insfrastructure/config/envs.ts
@@ -5,5 +5,6 @@ export const envs = {
 
   PORT: get('PORT').required().asPortNumber(),
   POSTGRES_URL: get('POSTGRES_URL').required().asString(),
+  JWT_SECRET: get('JWT_SECRET').required().asString(),
 
 };
