86dc626141d9ff574aede8f22d58b301fd47b35d
Adrian <alextravc.342@gmail.com>
2023-11-11 16:05:19 -0500

    [ADD]: AuthRoutes inject AuthController

--- src/auth/infrastructure/rest/routes.ts
+++ src/auth/infrastructure/rest/routes.ts
@@ -1,33 +1,24 @@
 import { Router } from 'express';
 
-import { PostgresRoleDatasource, PostgresUserDatasource } from '../datasources';
-import { RoleRepositoryImpl, UserRepositoryImpl } from '../repositories';
 import { AuthController } from './controller';
 import { AuthValidatorMiddleware } from './middlewares';
 
 
 export class AuthRoutes {
 
-  static get routes(): Router {
+  ///* DI
+  constructor(private readonly authController: AuthController) {}
+
+  get routes(): Router {
     const router = Router();
 
-    ///* DI
-    const userDatasource = new PostgresUserDatasource();
-    const userRepository = new UserRepositoryImpl(userDatasource);
-    const roleDatasource = new PostgresRoleDatasource();
-    const roleRepository = new RoleRepositoryImpl(roleDatasource);
-
-    const authController = new AuthController(userRepository, roleRepository);
-
-    
     router.post(
       '/register',
       AuthValidatorMiddleware.registerRules(),
-      authController.register
+      this.authController.register
     );
 
-    router.post('/login', authController.login);
-
+    router.post('/login', this.authController.login);
 
     return router;
   }
