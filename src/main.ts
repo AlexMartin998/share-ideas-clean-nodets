4d4a03dbfc26041f6002954251f6fb03bb990f91
Adrian <alextravc.342@gmail.com>
2023-11-11 16:05:58 -0500

    [ADD]: main() inject AppRouter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { envs } from '@/shared/insfrastructure/config';
+import { diContainer, envs } from '@/shared/insfrastructure/config';
 import { AppRouter } from '@/shared/insfrastructure/server/router';
 import { Server } from '@/shared/insfrastructure/server/server';
+
 
 
 (async () => {
@@ -9,7 +10,8 @@ import { Server } from '@/shared/insfrastructure/server/server';
 
 
 function main() {
-
+  const AppRouter = diContainer.resolve<AppRouter>('AppRouter');
+  
   // Avoid hidden dependencies
   const server = new Server({
     port: envs.PORT,
