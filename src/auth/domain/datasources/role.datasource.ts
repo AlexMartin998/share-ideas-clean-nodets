f67cdc84a51619e572189347e557f42135e7b964
Adrian <alextravc.342@gmail.com>
2023-11-13 14:44:53 -0500

    [ADD]: datasources deleted

--- src/auth/domain/datasources/role.datasource.ts
+++
@@ -1,9 +0,0 @@
-import { Nullable } from '@/shared/domain';
-import { Role } from '../models/role';
-
-
-export interface RoleDatasource {
-
-  findByName(name: string): Promise<Nullable<Role>>;
-
-}
