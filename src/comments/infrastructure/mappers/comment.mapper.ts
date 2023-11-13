da3eeefb0496a2f53a95cb66f9cf22d0de7f40e7
Adrian <alextravc.342@gmail.com>
2023-11-13 12:31:51 -0500

    [ADD]: CommentMapper - domainModelToResponseDto

--- src/comments/infrastructure/mappers/comment.mapper.ts
+++ src/comments/infrastructure/mappers/comment.mapper.ts
@@ -1,17 +1,21 @@
 import { Comment } from '@/comments/domain/models';
+import { CommentResponseDto } from '../dtos';
+
 
 export class CommentMapper {
+
   public static entityToDomainModel(object: { [key: string]: any }): Comment {
     const { id, text, userId, ideaId } = object;
 
     return new Comment(id, text, userId, ideaId);
   }
 
-  // public static domainModelToResponseDto(object: {
-  //   [key: string]: any;
-  // }): IdeaResponseDto {
-  //   const { id, title, description, userId, comments } = object;
+  public static domainModelToResponseDto(object: {
+    [key: string]: any;
+  }): CommentResponseDto {
+    const { id, text, userId, ideaId } = object;
 
-  //   return IdeaResponseDto.create({ id, title, description, userId, comments });
-  // }
+    return CommentResponseDto.create({ id, text, userId, ideaId });
+  }
+
 }
