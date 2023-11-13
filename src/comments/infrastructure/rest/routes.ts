ecb8448be1f8b6c07efdfb5f93394fb1ef88fadd
Adrian <alextravc.342@gmail.com>
2023-11-13 12:33:03 -0500

    [ADD]: CommentsRoutes - create

---
+++ src/comments/infrastructure/rest/routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+
+import { AuthMiddleware } from '@/shared/insfrastructure/middlewares';
+import { CommentsController } from './controller';
+
+export class CommentsRoutes {
+  ///* DI
+  constructor(
+    private readonly commentsController: CommentsController,
+    private readonly authMiddleware: AuthMiddleware
+  ) {}
+
+  get routes(): Router {
+    const router = Router();
+
+    router.post(
+      '/',
+      [this.authMiddleware.validateJWT],
+      this.commentsController.create
+    );
+
+    return router;
+  }
+}
--- src/shared/insfrastructure/server/router.ts
+++ src/shared/insfrastructure/server/router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { AuthRoutes } from '@/auth/infrastructure/rest/routes';
 import { IdeasRoutes } from '@/ideas/infrastructure/rest/routes';
+import { CommentsRoutes } from '@/comments/infrastructure/rest/routes';
 
 
 export class AppRouter {
@@ -9,7 +10,8 @@ export class AppRouter {
   ///* DI
   constructor(
     private readonly authRoutes: AuthRoutes,
-    private readonly ideasRoutes: IdeasRoutes
+    private readonly ideasRoutes: IdeasRoutes,
+    private readonly commentsRoutes: CommentsRoutes
   ) {}
 
 
@@ -18,7 +20,7 @@ export class AppRouter {
 
     router.use('/api/auth', this.authRoutes.routes);
     router.use('/api/ideas', this.ideasRoutes.routes);
-    // router.use('/api/comments', this.commentsRoutes.routes);
+    router.use('/api/comments', this.commentsRoutes.routes);
 
     return router;
   }
